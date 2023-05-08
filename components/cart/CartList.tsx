0d6bf6fdd6688c1f3a4821482a067f44862a00b4
NhanNguyen179 <nhan1709rt@gmail.com>
2023-05-08 22:14:57 +0700

    update history aution

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,65 +1,92 @@
-import { Box, CardActionArea, CardMedia, Divider, Grid, IconButton, Link, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Divider,
+  Grid,
+  IconButton,
+  Link,
+  Typography,
+} from "@mui/material";
 
-import NextLink from 'next/link';
-import { ItemCounter } from '../ui';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { FC, useContext } from 'react';
-import { CartContext } from '../../context/cart/CartContext';
-import { ICartProduct } from '../../interfaces';
+import NextLink from "next/link";
+import { ItemCounter } from "../ui";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import { FC, useContext } from "react";
+import { CartContext } from "../../context/cart/CartContext";
+import { ICartProduct } from "../../interfaces";
+import { seedData } from "../../database";
 
-interface Props{
+interface Props {
   editable?: boolean;
 }
 
-export const CartList:FC<Props> = ({editable = false}) => {
-
-
-  const { cart:cartProducts, updateCartQuantity, removeCartProduct  } = useContext(CartContext);
-
-  const onNewCartQuantityValue = (product: ICartProduct, newQuantityValue: number) => {
-    product.quantity = newQuantityValue
-    updateCartQuantity(product)
-  }
-  const removeProductInCart = (product: ICartProduct) => {
-    removeCartProduct(product)
-  }
-
+export const CartList: FC<Props> = ({ editable = false }) => {
+  const cartProducts = seedData.initialData.products;
   return (
     <>
-      {
-        cartProducts.map( product => (
-          <Grid key={product.slug  } container spacing={2} sx={{ mb:1 }}>
-            <Grid item xs={3}>
-              <NextLink href={`/product/${product.slug}`} passHref>
-                <Link>
-                  <CardActionArea>
-                    <CardMedia
-                        image={`/products/${product.image}`}
-                        component='img'
-                        sx={{ borderRadius: '5px' }}
-                      />
-                  </CardActionArea>
-                </Link>
-              </NextLink>
-
-            </Grid>
-            <Grid item xs={10}>
-              <Box display='flex' flexDirection='column'>
-                <Typography variant='body1'>{product.title}</Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={2} display='flex' flexDirection='column' alignItems='center'>
-              <Typography variant='subtitle1'>{`$${product.price}`}</Typography>
-            </Grid>
-            <Divider  sx={{
-                  width: '80%',
-                  bgcolor: 'background.paper',
-                  margin: '10px auto',
-                  px:2
-                }}/>
+      {cartProducts.map((product) => (
+        <Grid key={product.slug} container spacing={2} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            <NextLink href={`/product/${product.slug}`} passHref>
+              <Link>
+                <CardActionArea>
+                  <CardMedia
+                    image={`/products/${product.images[0]}`}
+                    component="img"
+                    sx={{ borderRadius: "5px" }}
+                  />
+                </CardActionArea>
+              </Link>
+            </NextLink>
           </Grid>
-        ) )
-      }
+          <Grid item xs={9}>
+            <Grid key={product.slug} container spacing={2} sx={{ mb: 1 }}>
+              <Grid
+                item
+                xs={9}
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+              >
+                <Box display="flex" flexDirection="column">
+                  <Typography variant="h6">{product.name}</Typography>
+                </Box>
+              </Grid>
+              <Grid
+              item
+              xs={3}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <Typography variant="subtitle1">{`$${product.price}`}</Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <Button color="info">Danh sách người đấu giá</Button>
+            </Grid>
+            </Grid>
+           
+          </Grid>
+
+          <Divider
+            sx={{
+              width: "80%",
+              bgcolor: "background.paper",
+              margin: "10px auto",
+              px: 2,
+            }}
+          />
+          <br></br>
+        </Grid>
+      ))}
     </>
-  )
-}
+  );
+};
--- components/cart/OrderSummary.tsx
+++ components/cart/OrderSummary.tsx
@@ -1,39 +1,36 @@
 import { Divider, Grid, Typography } from "@mui/material"
-import { useContext } from "react"
-import { CartContext } from "../../context/cart/CartContext";
 import { currency } from "../../utils";
 export const OrderSummary = () => {
-  const {numberOfItems, subTotal, total, tax} = useContext(CartContext);
   return (
    <Grid container>
      <Grid item xs={6}>
        <Typography>Số lượng sản phẩm:</Typography>
      </Grid>
      <Grid item xs={6} display='flex' justifyContent='end'>
-       <Typography>{numberOfItems} </Typography>
+       <Typography>1 </Typography>
      </Grid>
      <Grid item xs={6}>
        <Typography>Tổng cộng:</Typography>
      </Grid>
      <Grid item xs={6} display='flex' justifyContent='end'>
-       <Typography>{currency.format(subTotal)}</Typography>
+       <Typography>10000$</Typography>
      </Grid>
      <Grid item xs={6}>
        <Typography>Thuế ({Number(process.env.NEXT_PUBLIC_TAX_RATE) * 100 || 0}%) :</Typography>
      </Grid>
      <Grid item xs={6} display='flex' justifyContent='end'>
-       <Typography>{currency.format(tax)}</Typography>
+       <Typography>10000$</Typography>
      </Grid>
      <Divider  sx={{
                   width: '100%',
                   bgcolor: 'background.paper',
                   mt:2
                 }}/>
-     <Grid item xs={6} sx={{mt:2}}>
-       <Typography variant='subtitle1' >Tổng số phải trả:</Typography>
+     <Grid item xs={12} sx={{mt:2}} display='flex' justifyContent='center'>
+       <Typography variant='subtitle1' >Tổng số tiền đấu giá:</Typography>
      </Grid>
-     <Grid item xs={6} display='flex' justifyContent='end'>
-       <Typography variant='subtitle1' >{currency.format(total)}</Typography>
+     <Grid item xs={12} display='flex' justifyContent='center'>
+       <Typography variant='subtitle1' >10000$</Typography>
      </Grid>
    </Grid>
   )

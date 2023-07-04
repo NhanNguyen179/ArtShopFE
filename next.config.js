3cbd61ed6369a7690119f7d68560bf21bacc809b
NhanNguyen179 <nhan1709rt@gmail.com>
2023-07-04 20:19:50 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    ACTIVITY_TRACKING_URL: 'https://activity-tracking-art-shop.vercel.app/',
+    ACTIVITY_TRACKING_URL: 'https://activity-tracking-art-shop.vercel.app/event-tracking/',
     ENABLE_ACTIVITY_TRACKING: "true",
     IMAGE_DOMAIN:"https://ec2-34-198-71-168.compute-1.amazonaws.com:8000",
   },
--- src/activity-tracking/ActivityTrackingService.ts
+++ src/activity-tracking/ActivityTrackingService.ts
@@ -24,7 +24,7 @@ const saveActivityEvent = async (event: BaseEvent) => {
         data: event,
       };
       await axios.post(
-        `${ACTIVITY_TRACKING_URL}/${activityTrackingRoute}`,
+        `${ACTIVITY_TRACKING_URL}${activityTrackingRoute}`,
         requestOptions
       );
     }

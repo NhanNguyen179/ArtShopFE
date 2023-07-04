7595bb4537a769a5c428ea964c75abc4c4c7c9f4
NhanNguyen179 <nhan1709rt@gmail.com>
2023-07-04 21:00:31 +0700

    update

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ const nextConfig = {
     ENABLE_ACTIVITY_TRACKING: "true",
     IMAGE_DOMAIN: "https://ec2-34-198-71-168.compute-1.amazonaws.com:8000",
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig

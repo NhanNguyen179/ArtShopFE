6b4da08c582c6079673ba88c84853c88836dd039
NhanNguyen179 <nhan1709rt@gmail.com>
2023-07-04 20:35:31 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,9 +4,22 @@ const nextConfig = {
   env: {
     ACTIVITY_TRACKING_URL: 'https://activity-tracking-art-shop.vercel.app/event-tracking/',
     ENABLE_ACTIVITY_TRACKING: "true",
-    IMAGE_DOMAIN:"https://ec2-34-198-71-168.compute-1.amazonaws.com:8000",
+    IMAGE_DOMAIN: "https://ec2-34-198-71-168.compute-1.amazonaws.com:8000",
   },
-
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
